074f53d69c765c577863e20b1599691b8646fd3d
Marius Herget <info@herget.design>
2018-09-09 18:57:23 +0200

    Working vpp example. Updated Cmake

---
+++ src/cimarron-main.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <opencv2/highgui/highgui.hpp>
+
+#include <vpp/utils/opencv_bridge.hh>
+#include <vpp/vpp.hh>
+
+int main(int argc, char *argv[]) {
+  using namespace vpp;
+
+  if (argc != 2) {
+    std::cerr << "Usage : " << argv[0] << " image" << std::endl;
+    return 1;
+  }
+
+  typedef image2d<vuchar3> I;
+  I A = clone(from_opencv<vuchar3>(cv::imread(argv[1])), vpp::_border = 1);
+  I B(A.domain());
+
+  // Parallel Loop over pixels of in and out.
+  pixel_wise(relative_access(A), B) | [](auto n, auto &b) {
+    vint3 sum = vint3::Zero();
+
+    sum += n(0, -1).template cast<int>();
+    sum += n(0, 0).template cast<int>();
+    sum += n(0, 1).template cast<int>();
+
+    // Write the sum to the output image.
+    b = (sum / 3).cast<unsigned char>();
+  };
+
+  cv::imwrite("b.ppm", to_opencv(B));
+}
--- src/cimarron-main.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include <vpp/vpp.hh>
-
-int main(int argc, char *argv[]) {
-  std::cout << "Hello World!" << std::endl;
-  return 0;
-}
